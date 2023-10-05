99706420dda932755f094b636534bb13cc46cab1
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-10-05 14:57:05 +0200

    Initialized project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<div>
+			{/* Navbar */}
+			<div className="px-6 md:px-12">
+				{/* MainContent */}
+				<hr />
+				{/* Footer */}
+			</div>
+		</div>
+	);
+}
+
+export default App;
