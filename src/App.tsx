d4a37c7ce4a2c6e27e4316d41fc598065f9679ea
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-10-05 17:21:24 +0200

    Updated color palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './utils/MuiTheme';
 import Footer from './components/Footer';
 import MainContent from './components/MainContent';
 import Navbar from './components/Navbar';
 
 function App() {
 	return (
-		<div>
-			<Navbar />
-			<div className="px-6 md:px-12">
-				<MainContent />
-				<hr />
-				<Footer />
+		<ThemeProvider theme={theme}>
+			<div>
+				<Navbar />
+				<div className="px-6 md:px-12">
+					<MainContent />
+					<hr />
+					<Footer />
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 }
 
--- src/utils/MuiTheme.tsx
+++ src/utils/MuiTheme.tsx
@@ -1,6 +1,16 @@
-import { createTheme } from '@mui/material';
+import { PaletteColor, PaletteColorOptions, createTheme } from '@mui/material';
 
-const theme = createTheme({
+declare module '@mui/material/styles' {
+	interface Palette {
+		neutral: PaletteColor;
+	}
+
+	interface PaletteOptions {
+		neutral: PaletteColorOptions;
+	}
+}
+
+let theme = createTheme({
 	palette: {
 		primary: {
 			main: '#3376b9',
@@ -12,6 +22,36 @@ const theme = createTheme({
 			main: '#d6e6f1',
 			light: '#f2f7fa',
 			dark: '#a7c8e3',
+			contrastText: '#000',
+		},
+		error: {
+			main: '#f44336',
+			light: '#e57373',
+			dark: '#d32f2f',
+			contrastText: '#fff',
+		},
+		warning: {
+			main: '#ff9800',
+			light: '#ffb74d',
+			dark: '#f57c00',
+			contrastText: '#fff',
+		},
+		info: {
+			main: '#2196f3',
+			light: '#64b5f6',
+			dark: '#1976d2',
+			contrastText: '#fff',
+		},
+		success: {
+			main: '#4caf50',
+			light: '#81c784',
+			dark: '#388e3c',
+			contrastText: '#fff',
+		},
+		neutral: {
+			main: '#f2f2f2',
+			light: '#fff',
+			dark: '#d9d9d9',
 			contrastText: '#000',
 		},
 	},
