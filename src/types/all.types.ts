8e7edefba369a3c43ac8dd0a4de24dc555e4bbea
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-10-06 02:31:20 +0200

    Defined Supabase data structure

---
+++ src/types/all.types.ts
@@ -0,0 +1,14 @@
+export interface ChepData {
+	id: number;
+	equipment_type: string;
+	volume: number;
+	order_number: string;
+	deliver_number: number;
+	flare: boolean;
+	pie: number;
+	delivery: string;
+	warehouse: string;
+	comment: string;
+	status: string;
+	time: string;
+}
