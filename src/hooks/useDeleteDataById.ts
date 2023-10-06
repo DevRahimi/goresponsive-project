d9b4cba3b283e2b703340d31b8474e54c6c4e1d1
Ahmed Rahimi <ahmedmrahimi@gmail.com>
2023-10-06 08:30:00 +0200

    Adjusted hook code

--- src/hooks/useDeleteDataById.ts
+++ src/hooks/useDeleteDataById.ts
@@ -1,30 +1,22 @@
-import { useEffect, useState } from 'react';
 import supabase from '../services/supabase';
 
-const useDeleteDataById = (id: number) => {
-	const [error, setError] = useState('');
+const useDeleteDataById = async (id: number) => {
+	try {
+		const { error } = await supabase
+			.from('mychep_data')
+			.delete()
+			.match({ id });
 
-	useEffect(() => {
-		if (!id) return; // Return early if ID is not provided
-		deleteData();
-	}, []);
-
-	const deleteData = async () => {
-		try {
-			const { error } = await supabase
-				.from('mychep_data')
-				.delete()
-				.match({ id });
-
-			if (error) {
-				throw new Error(error.message);
-			}
-		} catch (error: Error | unknown) {
-			setError((error as Error).message);
+		if (error) {
+			throw new Error(error.message);
+		} else {
+			window.location.reload();
+			return;
 		}
-	};
-
-	return { error };
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
 };
 
 export default useDeleteDataById;
